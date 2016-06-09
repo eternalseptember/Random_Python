a9e8a2a9a746dc55640626c8430cc42884eb5630
KQ <eternal.september@gmail.com>
2016-06-09 19:34:35 -0400

    more shuffling algorithms

--- gambling.py
+++ gambling.py
@@ -1,6 +1,6 @@
 import random
 
-
+'''
 # Create a black box object that generates random numbers
 rng = random.random()
 print (rng)
@@ -15,3 +15,46 @@ print (r_odd)
 cards = list(range(52))
 random.shuffle(cards)
 print(cards)
+'''
+
+def make_random_ints(num, lower_bound, upper_bound):
+	""" 
+	Generate a list containing num random ints between lower_bound
+	and upper_bound. upper_bound is an open bound.
+	"""
+	rng = random.Random()
+	result = []
+	for i in range(num):
+		result.append(rng.randrange(lower_bound, upper_bound))
+	return result
+
+
+def make_random_ints_no_dups(num, lower_bound, upper_bound):
+	"""
+	Generate a list containing num random ints between
+	lower_bound and upper_bound. upper_bound is an open bound.
+	The result list cannot contain duplicates.
+	"""
+	result = []
+	rng = random.Random()
+	for i in range(num):
+		while True:
+			candidate = rng.randrange(lower_bound, upper_bound)
+			if candidate not in result:
+				break
+		result.append(candidate)
+	return result
+
+
+
+random_integers = make_random_ints(5, 1, 13) # Pick 5 random month numbers.
+print("{0}".format(random_integers))
+
+xs = list(range(1,13))		# Make list 1..12 (there are no duplicates)
+rng = random.Random()		# Make a random number generator
+rng.shuffle(xs)				# Shuffle the list
+result = xs[:5]				# Take the first five elements
+print(result)
+
+xs = make_random_ints_no_dups(5, 1, 10000000)
+print(xs)
