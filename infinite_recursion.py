2c58990083dea6dab640c88753c865e76be73963
KQ <eternal.september@gmail.com>
2016-07-10 13:28:36 -0400

    try finally

--- infinite_recursion.py
+++ infinite_recursion.py
@@ -1,6 +1,10 @@
 def recursion_depth(number):
-	print("{0}, ".format(number), end="")
-	recursion_depth(number + 1)
+	print("Recursion depth number", number)
+	try:
+		recursion_depth(number + 1)
+	except:
+		print("I cannot go any deeper into this wormhole.")
+		# Stops after r997
 
 
 
