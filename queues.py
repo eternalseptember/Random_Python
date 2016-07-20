89f3c9c56420d5a6923294e13d13c906f931db6b
KQ <eternal.september@gmail.com>
2016-07-20 13:25:59 -0400

    priority queue

--- queues.py
+++ queues.py
@@ -37,3 +37,62 @@ class Queue:
 		return cargo
 
 
+
+class ImprovedQueue:
+	def __init__(self):
+		self.length = 0
+		self.head = None
+		self.last = None
+
+	def is_empty(self):
+		return self.length == 0
+
+	def insert(self, cargo):
+		node = Node(cargo)
+		if self.length == 0:
+			# If list is empty, the new node is head and last
+			self.head = self.last = node
+		else:
+			# Find the last node
+			last = self.last
+			# Append the new node
+			last.next = node
+			self.last = node
+		self.length += 1
+
+	def remove(self):
+		cargo = self.head.cargo
+		self.head = self.head.next
+		self.length -= 1
+		if self.length == 0:
+			self.last = None
+		return cargo
+
+
+class PriorityQueue:
+	def __init__(self):
+		self.items = []
+
+	def is_empty(self):
+		return not self.items
+
+	def insert(self, item):
+		self.items.append(item)
+
+	def remove(self):
+		maxi = 0
+		for i in range(1, len(self.items)):
+			if self.items[i] > self.items[maxi]:
+				maxi = i
+		item = self.items[maxi]
+		del self.items[maxi]
+		return item
+
+
+
+q = PriorityQueue()
+for num in [11, 12, 14, 13]:
+	q.insert(num)
+
+while not q.is_empty():
+	print(q.remove())
