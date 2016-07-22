8a5dc60a1f73cb884c0713881277fa30e4b3c9a2
KQ <eternal.september@gmail.com>
2016-07-22 15:12:41 -0400

    tokens parsing

--- trees.py
+++ trees.py
@@ -41,12 +41,44 @@ def print_tree_indented(tree, level=0):
 	print_tree_indented(tree.left, level+1)
 
 
+def get_token(token_list, expected):
+	if token_list[0] == expected:
+		del token_list[0]
+		return True
+	return False
 
-tree = Tree("+", Tree(1), Tree("*", Tree(2), Tree(3)))
-print_tree_preorder(tree)
-print()
+
+def get_number(token_list):
+	if get_token(token_list, "("):
+		x = get_sum(token_list)	
+		if not get_token(token_list, ")"):
+			raise ValueError("Missing close parenthesis")
+		return x
+	else:
+		x = token_list[0]
+		if type(x) != type(0): return None
+		del token_list[0]
+		return Tree(x, None, None)
+
+
+def get_product(token_list):
+	a = get_number(token_list)
+	if get_token(token_list, "*"):
+		b = get_product(token_list)
+		return Tree("*", a, b)
+	return a
+
+
+def get_sum(token_list):
+	a = get_product(token_list)
+	if get_token(token_list, "+"):
+		b = get_sum(token_list)
+		return Tree("+", a, b)
+	return a
+
+
+
+
+token_list = [9, "*", "(", 11, "+", 5, ")", "*", 7, "end"]
+tree = get_sum(token_list)
 print_tree_postorder(tree)
-print()
-print_tree_inorder(tree)
-print()
-print_tree_indented(tree)
