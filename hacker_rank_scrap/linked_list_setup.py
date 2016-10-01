d4e4052680fe45ae2e1bb44a97f2a4c4bb17c615
KQ <eternal.september@gmail.com>
2016-09-30 20:04:21 -0400

    reversing a linked list without recursion

---
+++ hacker_rank_scrap/linked_list_setup.py
@@ -0,0 +1,27 @@
+class Node(object):
+	def __init__(self, data=None, next_node=None):
+		self.data = data
+		self.next = next_node
+
+	def __str__(self):
+		return str(self.data)
+
+
+def print_list(head):
+	if head is not None:
+		print(head.data)
+		print_list(head.next)
+
+
+
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
