ec48dd48fc1c1ec18d0084713b01d3fc04a6c0df
KQ <eternal.september@gmail.com>
2016-10-03 13:15:10 -0400

    removing duplicate values from linked list

--- hacker_rank/linked_list_merge_two_sorted.py
+++ hacker_rank/linked_list_merge_two_sorted.py
@@ -5,8 +5,8 @@ Node is defined as
 
 class Node(object):
 	def __init__(self, data=None, next_node=None):
-	self.data = data
-	self.next = next_node
+		self.data = data
+		self.next = next_node
 
 return back the head of the linked list in the below method.
 """
