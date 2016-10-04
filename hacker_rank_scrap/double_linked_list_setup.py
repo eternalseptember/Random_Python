eb2d4e546a1eee17947574ded77e0790f1dbe3b9
KQ <eternal.september@gmail.com>
2016-10-04 13:27:40 -0400

    insert value in sorted doubly linked list...

---
+++ hacker_rank_scrap/double_linked_list_setup.py
@@ -0,0 +1,29 @@
+class Node(object):
+	def __init__(self, data=None, next_node=None, prev_node=None):
+		self.data = data
+		self.next = next_node
+		self.priv = prev_node
+
+	def __str__(self):
+		return str(self.data)
+
+
+def print_list(head):
+	if head is not None:
+		print(head.data)
+		print_list(head.next)
+
+
+
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+node6 = Node(6)
+node7 = Node(7)
+
+
+
