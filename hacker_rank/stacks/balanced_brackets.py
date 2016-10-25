778caabcda1849ea29e355bf6fdb48df3d2aefc9
KQ <eternal.september@gmail.com>
2016-10-25 19:36:40 -0400

    solved the balanced brackets problem

--- hacker_rank/stacks/balanced_brackets.py
+++ hacker_rank/stacks/balanced_brackets.py
@@ -9,10 +9,33 @@ strings. Each line i of the n subsequent lines consists of a single
 string, s, denoting a sequence of brackets.
 """
 
+
 def is_balanced(str):
-	#
+	stack = []
+	open_brackets = ['{', '(', '[']
 
+	for char in str:
+		if char in open_brackets:
+			stack.append(char)
+		else:
+			if len(stack)> 0:
+				open_bracket = stack.pop()
+				if (open_bracket == '{') and (char == '}'):
+					continue
+				elif (open_bracket == '(') and (char == ')'):
+					continue
+				elif (open_bracket == '[') and (char == ']'):
+					continue
+				else:
+					return False
+			else:
+				return False
 
+	# after going through all of the characters in the string...
+	if len(stack) == 0:
+		return True
+	else:
+		return False
 
 
 t = 3
@@ -24,7 +47,7 @@ for i in range(t):
 	# s = input().strip()
 	s = in_str[i].strip()
 
-	if is_balanced(str):
+	if is_balanced(s):
 		print('YES')
 	else:
 		print('NO')
