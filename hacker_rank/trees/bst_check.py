644ae18c23e4e02fb84a9c68446b2f521c6d7aa2
KQ <eternal.september@gmail.com>
2016-10-28 21:40:32 -0400

    hooraay!!

--- hacker_rank/trees/bst_check.py
+++ hacker_rank/trees/bst_check.py
@@ -20,24 +20,23 @@ class Node(object):
 		return str(self.data)
 
 
-def check_binary_search_tree(root):
-	if root.left is not None:
-		if root.left.data < root.data:
-			check_binary_search_tree(root.left)
-		else:
-			return False
+def check_binary_search_tree_(root, min=0, max=10000):
+	if root is None:
+		return True
 
-	if root.right is not None:
-		if root.right.data > root.data:
-			check_binary_search_tree(root.right)
-		else:
-			return False
-
-	return True
-
+	#print("min: {0}, root:{1}, max: {2}".format(min, root, max))
+	if ((root.data > min) and
+		(root.data < max) and
+		(check_binary_search_tree_(root.left, min, root.data)) and
+		(check_binary_search_tree_(root.right, root.data, max))):
+		return True
+	else:
+		return False
+	
 
 
 
+# test case 1: no
 node1 = Node(1)
 node4 = Node(4)
 node5 = Node(5, node1, node4)
@@ -45,7 +44,44 @@ node6 = Node(6)
 node2 = Node(2, node6)
 node3 = Node(3, node5, node2)
 
-if check_binary_search_tree(node3):
+if check_binary_search_tree_(node3):
 	print("Yes")
 else:
 	print("No")
+print()
+
+# test case 2: no
+node2 = Node(2)
+node6 = Node(6)
+node7 = Node(7)
+node9 = Node(9)
+node3 = Node(3, node2, node6)
+node8 = Node(8, node7, node9)
+node5 = Node(5, node3, node8)
+
+if check_binary_search_tree_(node5):
+	print("Yes")
+else:
+	print("No")
+print()
+
+# test case 3: yes
+node1 = Node(1)
+node4 = Node(4)
+node3 = Node(3, node1, node4)
+node6 = Node(6)
+node5 = Node(5, node3, node6)
+node8 = Node(8)
+node10 = Node(10)
+node9 = Node(9, node8, node10)
+node13 = Node(13)
+node17 = Node(17)
+node15 = Node(15, node13, node17)
+node12 = Node(12, node9, node15)
+node7 = Node(7, node5, node12)
+
+if check_binary_search_tree_(node7):
+	print("Yes")
+else:
+	print("No")
+print()
