acb472ba92944e3c367906c9066c51e3e86d0a7e
KQ <eternal.september@gmail.com>
2016-11-01 22:05:32 -0400

    setting up test cases for insertion sort

---
+++ hacker_rank/sorting/insertion_sort.py
@@ -0,0 +1,28 @@
+"""
+Input Format:
+There will be two lines of input:
+1. the size of the array
+2. the unsorted array of integers
+
+Output Format:
+On each line, output the entire array every time an item is shifted in it.
+"""
+
+
+def insertion_sort(arr):
+	# takes an array with the value 'e' in the right-most cell
+
+
+
+
+
+in_1 = '5'
+in_2 = '2 4 6 8 3'
+
+# size = int(input().strip())
+# unsorted = [int(temp) for temp in input().split(' ')]
+
+size = int(in_1.strip())
+unsorted = [int(temp) for temp in in_2.split(' ')]
+
+insertion_sort(unsorted)
