ea49e4344e32925bb0cc6cccafa3bb38e13983fc
KQ <eternal.september@gmail.com>
2016-11-07 21:22:17 -0500

    setting up quick sort

---
+++ hacker_rank/sorting/quick_sort.py
@@ -0,0 +1,32 @@
+"""
+Input Format:
+The first line contains n (the size of ar).
+The second line contains  space-separated integers describing ar
+(the unsorted array). The first integer (corresponding to ar[0])
+is your pivot element, p.
+
+Output Format:
+On a single line, print the partitioned numbers (i.e.: the elements
+in left, then the elements in equal, and then the elements in right).
+Each integer should be separated by a single space.
+"""
+
+
+def quick_sort(arr):
+	# stuff here
+	
+
+
+
+
+# n = int(input().strip())
+# arr = [int(temp) for temp in input().split(' ')]
+
+n = '5'
+in_1 = '4 5 3 7 2'
+arr = [int(temp) for temp in in_1.split(' ')]
+# expected output: 3 2 4 5 7
+
+quick_sort(arr)
+
+
