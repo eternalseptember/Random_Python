decb8c51a78592f85ab33960718ee8e4c6ebc794
KQ <eternal.september@gmail.com>
2016-11-10 22:22:50 -0500

    still working on it

--- hacker_rank/sorting/quick_sort_2.py
+++ hacker_rank/sorting/quick_sort_2.py
@@ -4,12 +4,13 @@ Please maintain the original order of the elements in the left and right
 partitions while partitioning around a pivot element.
 
 For example: Partition about the first element for the array
-A[]={5, 8, 1, 3, 7, 9, 2} will be {1, 3, 2, 5, 8, 7, 9}
+A[] = {5, 8, 1, 3, 7, 9, 2} will be {1, 3, 2, 5, 8, 7, 9}
 
 Input Format:
 There will be two lines of input:
 *  n - the size of the array
 *  arr - the n numbers of the array
+Each number is unique.
 
 Output Format:
 Print every partitioned sub-array on a new line.
@@ -17,8 +18,34 @@ Print every partitioned sub-array on a new line.
 
 
 def quick_sort(arr):
+	left, p, right = partition(arr)
+	
+
+
+
+	sorted_list = left + p + right
+	print(*sorted_list, sep=' ')
+
+
+
+def partition(arr):
+	if (len(arr) <= 1):
+		return None
+
+	p = [arr.pop()]
+	left = []
+	right = []
+
 	for i in arr:
-		print(i, end=' ')
+		if i < p[0]:
+			left.append(i)
+		else:
+			right.append(i)
+
+	new_list = left + p + right
+	print(*new_list, sep=' ')
+
+	return left, p, right
 
 
 
@@ -30,4 +57,4 @@ n = 7
 in_1 = '5 8 1 3 7 9 2'
 arr = [int(temp) for temp in in_1.split(' ')]
 
-quick_sort(arr)
+partition(arr)
