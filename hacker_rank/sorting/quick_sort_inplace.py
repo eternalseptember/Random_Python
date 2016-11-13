5cc16ebb365afc859eb9e55331dc3e91f5ffae3a
KQ <eternal.september@gmail.com>
2016-11-13 12:15:19 -0500

    only passes the first test case

--- hacker_rank/sorting/quick_sort_inplace.py
+++ hacker_rank/sorting/quick_sort_inplace.py
@@ -14,9 +14,39 @@ Print the entire array on a new line at the end of every partitioning method.
 
 
 def quick_sort_inplace(arr):
-	# stuff here
+	pivot = partition(arr, 0, len(arr)-1)
+	#left_pivot = partition(arr, 0, pivot-1)
+	#right_pivot = partition(arr, pivot+1, len(arr)-1)
+
+
+def partition(arr, beg_index, pivot_index):
+	size = len(arr[beg_index:pivot_index + 1])
+	if size <= 1:
+		return None
+
+
+	pivot = arr[pivot_index]
+	larger_partition_index = None
+
+	for i in range(beg_index, pivot_index):
+		if arr[i] > pivot:
+			if larger_partition_index is None:
+				larger_partition_index = i
+		else:
+			# arr[i] < pivot
+			if larger_partition_index is not None:
+				arr[i], arr[larger_partition_index] = arr[larger_partition_index], arr[i]
+				larger_partition_index += 1
+
+	if larger_partition_index is not None:
+		arr[pivot_index], arr[larger_partition_index] = arr[larger_partition_index], arr[pivot_index]
+
 	print(*arr, sep=' ')
 
+	left = partition(arr, beg_index, larger_partition_index-1)
+	right = partition(arr, larger_partition_index+1, pivot_index)
+
+	return larger_partition_index
 
 
 
