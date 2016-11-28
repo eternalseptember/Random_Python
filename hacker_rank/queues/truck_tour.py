9d63f65255745cbb9617107f90d633fabb6bd0d8
KQ <eternal.september@gmail.com>
2016-11-28 15:47:48 -0500

    it passes some test cases

--- hacker_rank/queues/truck_tour.py
+++ hacker_rank/queues/truck_tour.py
@@ -21,24 +21,52 @@ pump from which we can start the tour.
 """
 
 
-def calc_fuel(amt, dist):
-	# do math here
+def calc_fuel(possible_start, station_list):
+	total_fuel = 0
+
+	for station_num in possible_start:
+		# build a circular station list
+		init1 = station_list[:station_num]
+		init2 = station_list[station_num:]
+		new_station_list = init2 + init1
+
+		# cycle through the new station list
+		for station in new_station_list:
+			amt = station[0]
+			dist = station[1]
+
+			total_fuel += (amt - dist)
+
+			if total_fuel < 0:
+				# this is not a correct starting station
+				break
+
+		if total_fuel < 0:
+			# continue to next possible starting station
+			continue
+		else:
+			return station_num
 
 
 
 # n = int(input().strip())
-station = None
+station_list = []    # tuples of (amt, dist)
+possible_start = []  # index of station_list
 
 # setting up test case 1
 n = 3
 in_str1 = ['1 5', '10 3', '3 4']
 
+
 for i in range(n):
 	# amt, dist = (int(temp) for temp in input().strip().split(' '))
 	amt, dist = (int(temp) for temp in in_str1[i].strip().split(' '))
+	station_list.append((amt, dist))
 
-	result = calc_fuel
-	# set the station number to i
+	if amt >= dist:
+		possible_start.append(i)
 
+
+station = calc_fuel(possible_start, station_list)
 print(station)
 # expected result: 1
