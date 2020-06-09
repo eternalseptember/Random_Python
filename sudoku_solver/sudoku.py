a37bc2922dd35d5a89086180c5b342c8c6908e7e
KQ <eternal.september@gmail.com>
2020-06-09 15:17:59 -0400

    trying to fill up the board with initial lists first

--- sudoku_solver/sudoku.py
+++ sudoku_solver/sudoku.py
@@ -5,12 +5,23 @@ from sudoku_solver import *
 sudoku = Sudoku_Solver()
 sudoku.import_board("puzzle_1.txt")
 sudoku.print_board()
+
+"""
 sudoku.check_box((0, 0))
 sudoku.print_possible_values()
 sudoku.test((5, 6), 8)
-
-
 print('\nafter inputting test value')
 sudoku.print_board()
+"""
+
+
+# try to get initial list of possible values first
+row = 0
+col = 0
+
+for y in range(3):
+	for x in range(3):
+		print()
+
 
 
