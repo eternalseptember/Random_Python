f6c9cc98884b8151058919a2fc80f127c489e4fc
KQ <eternal.september@gmail.com>
2020-09-14 12:58:54 -0400

    created a test files for easiest puzzles

---
+++ sudoku_solver/sudoku_test_1.py
@@ -0,0 +1,32 @@
+"""
+Test file for puzzle_1.
+Can be solved by checking all unique once.
+"""
+
+from sudoku_solver import *
+
+
+sudoku = Sudoku_Solver()
+sudoku.import_board("puzzle_1.txt")
+sudoku.print_board()
+print('===============================')
+
+
+# Solve
+print('Init reduce:')
+sudoku.init_reduce()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+
+print('Unique lookup test:')
+sudoku.check_all_unique()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+
+
+
+
+
+
