4771f6fc80393e20165418314b7d14279c935c07
KQ <eternal.september@gmail.com>
2020-09-15 10:10:03 -0400

    more test files

---
+++ sudoku_solver/sudoku_test_4.py
@@ -0,0 +1,34 @@
+"""
+Test file for puzzle_4.
+Not yet solved.
+"""
+
+from sudoku_solver import *
+
+
+sudoku = Sudoku_Solver()
+sudoku.import_board("puzzle_4.txt")
+sudoku.print_board()
+print('===============================')
+
+
+# Solve
+print('Init reduce:')
+sudoku.init_reduce()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+
+print('Check matching sets:')
+sudoku.check_matching_sets()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+
+
+
+
+
+
+
+
