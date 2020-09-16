c455884e7ba771e45734d40327ab7f325e0f77d4
KQ <eternal.september@gmail.com>
2020-09-16 11:55:07 -0400

    finished creating separate test files

---
+++ sudoku_solver/sudoku_test_funcs.py
@@ -0,0 +1,40 @@
+"""
+Testing smaller functions.
+"""
+
+from sudoku_solver import *
+
+
+# Test puzzles of various difficulty levels.
+sudoku = Sudoku_Solver()
+# sudoku.import_board("col_test.txt")
+# sudoku.import_board("row_test.txt")
+sudoku.import_board("check_within_box_row_test.txt")
+sudoku.print_board()
+print('===============================')
+
+
+
+# Testing steps
+print('Init reduce:')
+sudoku.init_reduce()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+"""
+print('Check matching sets:')
+sudoku.check_matching_sets()
+sudoku.print_board()
+sudoku.print_possible_values()
+"""
+
+print('Check within box:')
+sudoku.check_within_box()
+sudoku.print_board()
+sudoku.print_possible_values()
+
+
+
+
+
+
