77ac8bcb1d1b7267888e124cb2efc2ec00ec4406
KQ <eternal.september@gmail.com>
2021-01-13 19:04:50 -0500

    cleanup

--- sudoku_solver/sudoku_poss_elim.py
+++ sudoku_solver/sudoku_poss_elim.py
@@ -15,78 +15,18 @@ def check_within_boxes(self):
 	# Within each 3x3 box, tally up whether unfilled values fit within row.
 	# If so, then eliminate them as possibilities from neighboring boxes.
 
-	# keys: hashable string; value: dict containing info about missing vals
-	block_col_info = {}  # subdict keys: "num_missing", "in_cols", "in_boxes"
-
 	for i in [0, 3, 6]:  # i goes down.
-		block_row_info = {}  # subdict keys: "num_missing", "in_rows", "in_boxes"
-
 		for j in [0, 3, 6]:  # j goes across.
 			coord = (i, j)
-
-			# rows_list, cols_list = self.check_within_a_box(coord)
 			self.check_within_a_box(coord)
 
-			# Process info for block-row analysis.
-			# Create a hashable key out of the info given.
-			"""
-			for missing_val in rows_list.keys():
-				rows_str = '{0}-'.format(missing_val)
-				rows_str += ''.join(map(str, rows_list[missing_val]))
-
-				# Info about missing values.
-				if rows_str not in block_row_info:
-					block_row_info[rows_str] = {
-						'num_missing': missing_val,
-						'in_rows': rows_list[missing_val],
-						'in_boxes': [j]
-						}
-				else:
-					row_info = block_row_info[rows_str]
-					row_info['in_boxes'].append(j)
-			"""
-
-
-			# process info for block-col analysis.
-			# Create a hashable key out of the info given.
-			"""
-			for missing_val in cols_list.keys():
-				cols_str = '{0}-'.format(missing_val)
-				cols_str += ''.join(map(str, cols_list[missing_val]))
-
-				# info about missing values
-				if cols_str not in block_col_info:
-					block_col_info[cols_str] = {
-						'num_missing': missing_val,
-						'in_cols': cols_list[missing_val],
-						'in_boxes': [i]  # ??????
-					}
-				else:
-					col_info = block_col_info[cols_str]
-					col_info['in_boxes'].append(i)
-				"""
-
-
-		# Eliminate possibilities in this row's third box.
-		# self.remove_rows_in_box(block_row_info)
-
-
-	# Eliminate possibilities in each col's third box.
-	# self.remove_cols_in_box(block_col_info)
-
-	self.solve_queue()  # Not sure if this goes here.
-
-
+	self.solve_queue()
 
 
 def check_within_a_box(self, coord):
 	# coord defines the 3x3 box.
 	# Check within a single box to see whether missing values can be narrowed
 	# down to specific rows.
-	"""
-	rows_list = {}
-	cols_list = {}
-	"""
 
 	# Get the list of missing values and their possible locations in this box.
 	poss_vals_in_box = self.get_box_poss_vals(coord)
@@ -102,11 +42,7 @@ def check_within_a_box(self, coord):
 		# missing_val as possibilities in the rest of the row outside this box.
 		if len(in_rows_list) == 1:
 			self.remove_row_outside_box(missing_val, coord, in_rows_list[0])
-		"""
-		# Otherwise, collect info for block-level analysis.
-		elif len(in_rows_list) == 2:
-			rows_list[missing_val] = in_rows_list
-		"""
+
 
 		# Are they in the same col?
 		in_cols_list = self.in_which_cols(poss_locs_list)
@@ -115,15 +51,6 @@ def check_within_a_box(self, coord):
 		# missing_val as possibilities in the rest of the col outside this box.
 		if len(set(in_cols_list)) == 1:
 			self.remove_col_outside_box(missing_val, coord, in_cols_list[0])
-		"""
-		# Otherwise, collect info for block-level analysis.
-		elif len(in_cols_list) == 2:
-			cols_list[missing_val] = in_cols_list
-		"""
-
-	# Returns info for each individual 3x3 box.
-	# Use it to establish what needs to be eliminated in the remaining box.
-	# return rows_list, cols_list
 
 
 def in_which_rows(self, coords_list):
@@ -184,7 +111,6 @@ def remove_col_outside_box(self, eliminated_val, ref_box, in_col):
 		self.possible_vals_check(this_cell, eliminated_val)
 
 
-
 def remove_rows_in_box(self, block_info):
 	# Given info about a missing value and which two rows of which two boxes
 	# they're in, remove those possibilities in the third box.
@@ -215,7 +141,6 @@ def remove_rows_in_box(self, block_info):
 			for elim_val_in_this_row in in_rows:
 				this_coord = (elim_val_in_this_row, this_col)
 				self.possible_vals_check(this_coord, num_missing)
-
 
 
 def remove_cols_in_box(self, block_info):
@@ -252,16 +177,16 @@ def remove_cols_in_box(self, block_info):
 
 def check_block_elim(self):
 	# Check all nine boxes for patterns to eliminate possibilities.
-	# Within each 3x3 box, 
+	# Within each 3x3 box,
 	# tally up whether unfilled values fit within the same two rows.
 	# Then check neighboring boxes.
 	# By the process of elimination,
 	# deduce where that number is in the third row.
 
 	# keys: hashable string; value: dict containing info about missing vals
-	# subdict keys: "num_missing", "in_cols", "in_boxes"
-	block_row_info = {}  # subdict keys: "num_missing", "in_rows", "in_boxes"
-	block_col_info = {}  # pre-hashed?
+	# subdict keys: "num_missing", "in_cols" or "in_rows, "in_boxes"
+	block_row_info = {}
+	block_col_info = {}
 
 	for i in [0, 3, 6]:  # i goes down.
 
@@ -275,9 +200,8 @@ def check_block_elim(self):
 			for missing_val in poss_vals_in_box.keys():
 				poss_locs_list = poss_vals_in_box[missing_val]
 
-				# Are they in the same rows or cols?
+				# Are they in the same rows?
 				in_rows_list = self.in_which_rows(poss_locs_list)
-				in_cols_list = self.in_which_cols(poss_locs_list)
 
 				if len(in_rows_list) == 2:
 					# Create a hashable key.
@@ -295,6 +219,8 @@ def check_block_elim(self):
 						row_info['in_boxes'].append(j)
 
 
+				# Are they in the same cols?
+				in_cols_list = self.in_which_cols(poss_locs_list)
 
 				if len(in_cols_list) == 2:
 					# Create a hashable key.
@@ -312,9 +238,9 @@ def check_block_elim(self):
 						col_info['in_boxes'].append(i)
 
 
-
 	self.remove_rows_in_box(block_row_info)
 	self.remove_cols_in_box(block_col_info)
+	self.solve_queue()
 
 
 
