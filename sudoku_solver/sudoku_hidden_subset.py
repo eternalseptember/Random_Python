9dcb7eabf7681baa9d5bf15fc19a8986a0722cb7
KQ <eternal.september@gmail.com>
2021-05-10 18:34:20 -0400

    filling out functions

--- sudoku_solver/sudoku_hidden_subset.py
+++ sudoku_solver/sudoku_hidden_subset.py
@@ -195,7 +195,8 @@ def clean_hidden_sub_box(self, subset_info, box_row, box_col):
 			first_coord = subset_locs[0]
 
 			for i in range(3):  # row goes across
-				print()
+				for j in range(3):  # col goes down
+					print()
 
 
 
