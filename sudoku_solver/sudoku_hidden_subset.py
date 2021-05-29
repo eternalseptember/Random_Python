10124b009296c86cc55bf862bfc82d470e8eb34b
KQ <eternal.september@gmail.com>
2021-05-28 22:11:49 -0400

    error where values aren't being removed

--- sudoku_solver/sudoku_hidden_subset.py
+++ sudoku_solver/sudoku_hidden_subset.py
@@ -194,11 +194,13 @@ def remove_hidden_box(self, box_coord, subset_locs, subset_nums):
 			this_col = ref_col * 3 + j
 			this_coord = (this_row, this_col)
 
+			"""
 			print('this coord: {0}'.format(this_coord))
 			print('\tsubset locs: {0}'.format(subset_locs))
 			print('\tsubset nums: {0}'.format(subset_nums))
+			"""
 
-			# self.clean_hidden_subset(this_coord, subset_locs, subset_nums)
+			self.clean_hidden_subset(this_coord, subset_locs, subset_nums)
 
 
 
