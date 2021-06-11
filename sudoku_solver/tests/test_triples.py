022d4da007113d53276faffa8e2d86c0bc4a63a9
KQ <eternal.september@gmail.com>
2021-06-11 17:26:46 -0400

    naked triple

--- sudoku_solver/tests/test_triples.py
+++ sudoku_solver/tests/test_triples.py
@@ -1,5 +1,7 @@
 """
-
+Naked triple: 
+Not all cells must contain all three candidates, but there must not be more
+than three candidates in the three cells all together.
 """
 
 import sys
@@ -8,10 +10,17 @@ from sudoku_solver import *
 
 
 sudoku = Sudoku_Solver()
-sudoku.import_board("naked_triple.txt")
+sudoku.import_board("naked_triple_row.txt")
 sudoku.print_board()
 print('===============================')
 
+
+# Testing steps
+print('Init reduce:')
+sudoku.solve_queue()
+sudoku.print_board()
+sudoku.print_possible_values()
+print('===============================')
 
 
 
