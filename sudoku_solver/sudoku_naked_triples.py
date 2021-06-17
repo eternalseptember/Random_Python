a09244fc3edba4cebd7df0039be1c73b214e674e
KQ <eternal.september@gmail.com>
2021-06-17 19:09:33 -0400

    function for finding naked triple

--- sudoku_solver/sudoku_naked_triples.py
+++ sudoku_solver/sudoku_naked_triples.py
@@ -1,6 +1,9 @@
 # Import into the main sudoku_solver class.
-# Apparently, there's a difference between "naked triple" and "hidden triple".
-
+"""
+Naked triple: 
+Not all cells must contain all three candidates, but there must not be more
+than three candidates in the three cells all together.
+"""
 
 
 def check_naked_triples(self):
@@ -16,4 +19,15 @@ def check_naked_triples_col(self):
 	for i in range(9):
 		this_cell = (row, i)
 		self.set_lookup_table(this_cell, col_missing_vals)
+
+	self.find_naked_triple(col_missing_vals)
+
+
+
+def find_naked_triple(self, missing_vals):
+	return None
+
+
+
+
 
