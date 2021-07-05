a2bbb92ea3605f5ab2f3ee98acca769132766c38
KQ <eternal.september@gmail.com>
2021-07-04 22:42:51 -0400

    fixed an error

--- sudoku_solver/sudoku_naked_triples.py
+++ sudoku_solver/sudoku_naked_triples.py
@@ -47,11 +47,13 @@ def find_naked_triple(self, poss_trip_list):
 	# How to identify group?
 	triples = {}
 
-	for item in triples.keys():
+
+
+	for item in poss_trip_list.keys():
 		# decode the key for the coordinate first
 		coord_str = str(item)
 		coord = list(map(int, coord_str.split(',')))
-		# print(coord_str)
+		print(coord_str)
 
 		poss_vals = poss_trip_list[item]
 		print()
