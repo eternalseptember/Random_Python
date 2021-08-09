f0ef3c9146757aefab09d56d14b76ed8da973357
KQ <eternal.september@gmail.com>
2021-08-09 19:28:59 -0400

    filling out functions

--- sudoku_solver/sudoku_naked_triples.py
+++ sudoku_solver/sudoku_naked_triples.py
@@ -90,6 +90,10 @@ def check_naked_triples_col(self):
 		if this_cell in self.possible_values:
 			poss_vals = self.possible_values[this_cell]
 
+			# Can't be part of a triple if there are more than 3 candidates.
+			if len(poss_vals) > 3:
+				continue
+
 
 
 
