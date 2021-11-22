c0124e67df1fd5e459e10e586670bc9c43833f1d
KQ <eternal.september@gmail.com>
2021-11-22 15:36:07 -0500

    new test

--- sudoku_solver/tests/test_naked_triples.py
+++ sudoku_solver/tests/test_naked_triples.py
@@ -3,13 +3,18 @@ Naked triple:
 Not all cells must contain all three candidates, but there must not be more
 than three candidates in the three cells all together.
 
-TRIPLE ROW:
+TRIPLE ROW 1:
 [5,8,9] can only be in (4,3), (4,4), and (4,5);
 	but some numbers are missing in (4,4) and (4,5).
 
-TRIPLE COL: ??? ???
+TRIPLE COL 1:
 [5,8,9] can only be in (3,4), (4,4), and (5,4);
-	but some numbers are missing in (4,4) and (5,4).
+	but some numbers are missing in (4,4) and (5,4).\
+
+TRIPLE COL 2:
+[1,5,8] can only be in col 0.
+[2,3,8] can only be in col 8.
+Clear them from the rest of their boxes.
 """
 
 import sys
@@ -19,7 +24,8 @@ from sudoku_solver import *
 
 sudoku = Sudoku_Solver()
 # sudoku.import_board("naked_triple_row_1.txt")
-sudoku.import_board("naked_triple_col_1.txt")
+# sudoku.import_board("naked_triple_col_1.txt")
+sudoku.import_board("naked_triple_col_2.txt")
 sudoku.print_board()
 print('===============================')
 
