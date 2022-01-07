f82437dc1c0861d143deeffb940a69005c9e3587
KQ <eternal.september@gmail.com>
2022-01-06 20:58:43 -0500

    created test file

---
+++ sudoku_solver/tests/test_xwing.py
@@ -0,0 +1,20 @@
+"""
+
+"""
+
+
+import sys
+sys.path.append('../')
+from sudoku_solver import *
+
+
+# Test puzzles of various difficulty levels.
+sudoku = Sudoku_Solver()
+sudoku.import_board("xwing_1.txt")
+sudoku.print_board()
+print('===============================')
+
+
+
+
+
