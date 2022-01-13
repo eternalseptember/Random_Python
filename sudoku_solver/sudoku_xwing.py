1ed5095c6fcd83494d57cc26663b93afe9c2cfac
KQ <eternal.september@gmail.com>
2022-01-12 19:56:24 -0500

    simplifying some stuff

--- sudoku_solver/sudoku_xwing.py
+++ sudoku_solver/sudoku_xwing.py
@@ -5,15 +5,20 @@
 def check_xwing(self):
 	val_lookup = {}
 
-	for i in range(0, 8):  # i goes down
+
+	# for i in range(0, 8):  # i goes down
+	for i in [1]:
 		for j in range(0, 8):  # j goes across
 			this_coord = (i, j)
-			self.set_lookup_table(this_coord, val_lookup)
 
-			print('coord: {0}'.format(this_coord))
-			for poss_val in val_lookup.keys():
-				print('{0} - {1}'.format(poss_val, val_lookup[poss_val]))
-			print()
+			if this_coord in self.possible_values:
+				self.set_lookup_table(this_coord, val_lookup)
+
+				print('coord: {0}'.format(this_coord))
+				for poss_val in val_lookup.keys():
+					print('{0} - {1}'.format(poss_val, val_lookup[poss_val]))
+				print()
+
 
 
 
