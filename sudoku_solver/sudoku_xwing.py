a919214ded58330eb97e9c1a5b1f181e357b8c8f
KQ <eternal.september@gmail.com>
2022-01-14 16:00:15 -0500

    filling out functions

--- sudoku_solver/sudoku_xwing.py
+++ sudoku_solver/sudoku_xwing.py
@@ -1,13 +1,17 @@
 # Import into the main sudoku_solver class.
 
+# When there are
+# * only two possible cells for a value in each of two different rows,
+# * and these candidates lie also in the same columns,
+# then all other candidates for this value in the columns can be eliminated.
 
 
 def check_xwing(self):
-	val_lookup = {}
 
+	# for i in [1]:
+	for i in range(0, 8):  # i goes down
 
-	# for i in range(0, 8):  # i goes down
-	for i in [1]:
+		val_lookup = {}
 		for j in range(0, 8):  # j goes across
 			this_coord = (i, j)
 
@@ -15,14 +19,18 @@ def check_xwing(self):
 				self.set_lookup_table(this_coord, val_lookup)
 
 
-	for poss_val in val_lookup.keys():
-		print('{0} - {1}'.format(poss_val, val_lookup[poss_val]))
+		for poss_val in val_lookup.keys():
+			print('{0} - {1}'.format(poss_val, val_lookup[poss_val]))
+		print()
 
 
 
 
 
 def check_xwing_row(self, lookup_dict):
+	return None
+
+def xwing_is_same_col(self, coord1, coord2):
 	return None
 
 
