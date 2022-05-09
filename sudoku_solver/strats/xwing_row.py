5ad0c4e9c038f4707fff907cf3c6f5d84fe498a2
KQ <eternal.september@gmail.com>
2022-05-09 11:19:55 -0400

    not all values are getting passed for cleaning?

--- sudoku_solver/strats/xwing_row.py
+++ sudoku_solver/strats/xwing_row.py
@@ -31,10 +31,11 @@ def check_xwing_by_rows(self):
 		# End of row.
 
 		# testing
+		"""
 		print('lookup table at the end of row {0}'.format(i))
 		for poss_val in val_lookup_row.keys():
 			print('{0} - {1}'.format(poss_val, val_lookup_row[poss_val]))
-
+		"""
 
 		xwing_cands_row = self.check_xwing_cands(val_lookup_row)
 
@@ -54,6 +55,7 @@ def check_xwing_by_rows(self):
 	print('after removing entries that cannot be part of an xwing')
 	for poss_val in xwing_candidates.keys():
 		print('{0} - {1}'.format(poss_val, xwing_candidates[poss_val]))
+	print()
 
 
 	# Then check each dict entry to see if there's an xwing
@@ -61,6 +63,10 @@ def check_xwing_by_rows(self):
 	# can consolidate this into the eliminate entries list later
 	for poss_val in xwing_candidates.keys():
 		poss_coords = xwing_candidates[poss_val]
+
+
+		print('checking if xwing is in same cols:')
+		print('{0} - {1}'.format(poss_val, poss_coords))
 		xwing_set = self.check_xwing_is_same_cols(poss_val, poss_coords)
 
 
@@ -117,8 +123,6 @@ def clean_xwing_list(self, xwing_candidates):
 
 
 def check_xwing_is_same_cols(self, poss_val, list_of_coords):
-	print('{0} - {1}'.format(poss_val, list_of_coords))
-
 	xwing_set = []  # a list of a set
 
 	# check list_of_coords in groups of two
@@ -129,17 +133,17 @@ def check_xwing_is_same_cols(self, poss_val, list_of_coords):
 		row_1_coord_2 = list_of_coords[each_pair_1 + 1]
 		row_1_coords = (row_1_coord_1, row_1_coord_2)
 
-		# print('\t{0} {1}:'.format(row_1_coord_1, row_1_coord_2), end=' ')
+		print('\t{0} {1}:'.format(row_1_coord_1, row_1_coord_2), end=' ')
 
 		# check if there's more coords to compare to
 		if (each_pair_1 + 2) >= len(list_of_coords):
-			# print('no more coords to compare to')
+			print('no more coords to compare to')
 			break
 
 
 		# rest of coords to compare to
 		xwing_row_2_cands = list_of_coords[(each_pair_1 + 2):]
-		# print('{0}'.format(xwing_row_2_cands))
+		print('{0}'.format(xwing_row_2_cands))
 
 		for each_pair_2 in range(0, len(xwing_row_2_cands), 2):
 			row_2_coord_1 = xwing_row_2_cands[each_pair_2]
