402c2f1858a8658923eba80c6af93f06167ad611
KQ <eternal.september@gmail.com>
2022-05-29 16:25:00 -0400

    new xwing test

--- sudoku_solver/tests/test_xwing.py
+++ sudoku_solver/tests/test_xwing.py
@@ -5,6 +5,13 @@
     (0, 1) and (0, 4)
     (1, 1) and (1, 4)
     (5, 1) and (5, 4)
+
+
+	xwing_test_2:
+8 is locked to rows [1, 3] and cols [4, 6].
+8 as a possible value is removed from:
+
+
 """
 
 
@@ -15,7 +22,8 @@ from sudoku_solver import *
 
 # Test puzzles of various difficulty levels.
 sudoku = Sudoku_Solver()
-sudoku.import_board("test_boards/xwing_test_1.txt")
+# sudoku.import_board("test_boards/xwing_test_1.txt")
+sudoku.import_board("test_boards/xwing_test_2.txt")
 sudoku.print_board()
 print('===============================')
 
