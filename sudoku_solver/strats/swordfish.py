97a1b4abfc7a3e5633e53f579cc9d42b9b271742
KQ <eternal.september@gmail.com>
2022-07-04 18:38:06 -0400

    pointing elim was duplicative, implementing swordfish

---
+++ sudoku_solver/strats/swordfish.py
@@ -0,0 +1,13 @@
+"""
+xwing is 2x2.
+swordfish is 3x3.
+"""
+
+
+def check_swordfish(self):
+	return None
+
+
+
+
+
--- sudoku_solver/sudoku_solver.py
+++ sudoku_solver/sudoku_solver.py
@@ -40,7 +40,7 @@ class Sudoku_Solver():
 	from strats.xwing_col import check_xwing_by_cols, \
 	    check_xwing_is_same_rows, is_xwing_same_rows, clean_xwing_row
 
-	from strats.pointing_elim import pointing_elim
+	from strats.swordfish import check_swordfish
 
 
 
