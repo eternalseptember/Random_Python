bf75d62a531af94a3061845640253aae92daabcd
KQ <eternal.september@gmail.com>
2022-07-06 17:40:43 -0400

    setting up test file

--- sudoku_solver/tests/test_swordfish.py
+++ sudoku_solver/tests/test_swordfish.py
@@ -8,6 +8,19 @@ sys.path.append('../')
 from sudoku_solver import *
 
 
+# Test puzzles of various difficulty levels.
+sudoku = Sudoku_Solver()
+sudoku.import_board("test_boards/swordfish.txt")
+sudoku.print_board()
+print('===============================')
+
+
+
+
+
+
+
+
 
 
 
