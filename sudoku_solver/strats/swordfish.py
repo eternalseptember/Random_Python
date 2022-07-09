23099d7c667041e774d35efa1d8c19416ba96e00
KQ <eternal.september@gmail.com>
2022-07-09 16:13:51 -0400

    filling out swordfish function

--- sudoku_solver/strats/swordfish.py
+++ sudoku_solver/strats/swordfish.py
@@ -5,7 +5,13 @@ swordfish is 3x3.
 
 
 def check_swordfish(self):
-	return None
+
+	for i in range(9):  # i goes down
+		for j in range(9):  # j goes across
+			this_coord = (i, j)
+
+			# set lookup table
+			# but eliminate vals with fewer than 3 possible locations
 
 
 
